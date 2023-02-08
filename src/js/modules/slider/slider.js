8072d796e8194ba5014409f178775f3d88e9c964
lelikone777 <2257855@gmail.com>
2023-02-08 19:23:44 +0300

    mainSlider

--- src/js/modules/slider/slider.js
+++ src/js/modules/slider/slider.js
@@ -1,5 +1,10 @@
 export default class Slider {
-    constructor(page, btns){
+    constructor({
+                    page = '', 
+                    btns = '', 
+                    next = '', 
+                    prev = '' 
+    } = {}){
         this.page = document.querySelector(page);
         this.slides = this.page.children;
         this.btns = document.querySelectorAll(btns);
