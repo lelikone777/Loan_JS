2b5ab6a5aaac88baac106b9f3bc4c78dc62cf260
lelikone777 <2257855@gmail.com>
2023-02-09 17:28:51 +0300

    123

--- src/js/1.js
+++ src/js/1.js
@@ -8,46 +8,38 @@ window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({btns: '.next', container: '.page'});
     slider.render();
 
-    const modulePageSlider = new MiniSlider ({
-       container: '.moduleapp',
-        btns: '.next'
-    });
+    const modulePageSlider = new MainSlider({container: '.moduleapp', btns: '.next'});
     modulePageSlider.render();
 
-
-
-    // showUpSlider
     const showUpSlider = new MiniSlider({
         container: '.showup__content-slider',
         prev: '.showup__prev',
         next: '.showup__next',
-        activeClass: '.card-active',
-        animate: true,
-    })
+        activeClass: 'card-active',
+        animate: true
+    });
     showUpSlider.init();
 
-    // modulesSlider
     const modulesSlider = new MiniSlider({
         container: '.modules__content-slider',
         prev: '.modules__info-btns .slick-prev',
         next: '.modules__info-btns .slick-next',
-        activeClass: '.card-active',
+        activeClass: 'card-active',
         animate: true,
-        autoplay: true,
-    })
+        autoplay: true
+    });
     modulesSlider.init();
 
-    // feedSlider
     const feedSlider = new MiniSlider({
         container: '.feed__slider',
         prev: '.feed__slider .slick-prev',
         next: '.feed__slider .slick-next',
-        activeClass: 'feed__item-active',
-    })
+        activeClass: 'feed__item-active'
+    });
     feedSlider.init();
 
-    new VideoPlayer('.showup .play', '.overlay').init();
-    new VideoPlayer().init();
+    const player = new VideoPlayer('.showup .play', '.overlay');
+    player.init();
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
     new Form('.form').init();
